d14552970a45ae27efe4be8cdee18e6e7080a2ea
ssmbc2929_bartok <ssmbc2929bartok@icloud.com>
2026-01-15 11:53:26 +0900

    累積和を扱うファイルだよ
    累積和の操作をまとめているよ

--- algorithms/cpp/cumulative_sum/1-dimentional.cpp
+++ algorithms/cpp/cumulative_sum/1-dimentional.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 // 競プロ用
 // #include <bits/stdc++.h>
+
+/*１次元累積和を計算するためのファイルだよ*/
+/*    様々なことに活用してくださいな     */
+/*   質問・訂正などはXまでお願いします   */
+/*       アカウントなどはGithubに       */
+
 using namespace std;
 using sumType = int;  // 状況に応じて変更してね
 
@@ -16,4 +22,24 @@ void initCumulativeSum(sumType size) {
 // 値を入れるための関数
 void input(sumType index, sumType inp) {
   gCmlSum[index] = gCmlSum[index - 1] + inp;
+}
+
+// 全区間累積和を計算する関数
+void calcAllSum() {
+  for (int i = 1; i <= gCmlSum.size(); i++) {
+    gCmlSum[i] = gCmlSum[i] + gCmlSum[i - 1];
+  }
+}
+
+// 区間[a, b]の累積和を計算する関数
+void calcSecSum(int a, int b, bool isUp) {  // isUp = 1:aからbまで昇順, 2:bからaまで降順
+  if (isUp) {
+    for (int i = a + 1; i <= b; i++) {
+      gCmlSum[i] = gCmlSum[i] + gCmlSum[i - 1];
+    }
+  } else {
+    for (int i = b - 1; i >= a; i--) {
+      gCmlSum[i] = gCmlSum[i] + gCmlSum[i + 1];
+    }
+  }
 }
