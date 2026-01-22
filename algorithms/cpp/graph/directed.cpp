6ab9ef0d9bb72a5952663ced0ed2ebfef18cbba4
ssmbc2929_bartok <ssmbc2929bartok@icloud.com>
2026-01-22 21:00:47 +0900

    add.DFS 深さ優先探索のひな形を追加したよ2026/1/22

--- algorithms/cpp/graph/directed.cpp
+++ algorithms/cpp/graph/directed.cpp
@@ -2,8 +2,22 @@
 using namespace std;
 
 using Graph = vector<vector<int>>;
+vector<bool> seen;
 Graph G;
 
 void insert(int V, int ins) {
   G[V].push_back(ins);
+}
+
+//深さ優先探索
+//使用前にseen.assign(要素数, false);を記述すること
+void DFS(const Graph &G, int v){
+  seen[v] = true;
+
+  for(auto next_v : G[v]){
+    if(seen[next_v]){
+      continue;
+    }
+    DFS(G, next_v);
+  }
 }
