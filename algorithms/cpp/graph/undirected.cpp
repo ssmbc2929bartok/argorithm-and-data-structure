5cfd01fbb40b817882395e78771c8d684f98acba
ssmbc2929_bartok <ssmbc2929bartok@icloud.com>
2026-01-22 22:30:03 +0900

    add.show() グラフの内部を連結リスト表現で出力するshow関数を追加 その他細かい部分の調整 2026/1/22

--- algorithms/cpp/graph/undirected.cpp
+++ algorithms/cpp/graph/undirected.cpp
@@ -1,10 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-using Graph = vector<vector<int>>;
+using Graph = vector<vector<int>>;  // main関数内などで（全体の大きさ + 1）のサイズを確保すること
 Graph G;
 
-void insert(int V, int ins) {
+void insert(Graph& G, int V, int ins) {
   G[V].push_back(ins);
   G[ins].push_back(V);
+}
+
+// 連結リストを表示する
+//  頂点の表示順を昇順にしたいときはsort(G[i].begin(), G[i].end());などを先に実行すること
+void show(const Graph& G, int V) {
+  for (int next_v : G[V]) {
+    cout << next_v << " ";
+  }
+  cout << endl;
+}
+
+vector<bool> seen;
+// 深さ優先探索
+// 使用前にseen.assign(要素数, false);を記述すること
+void DFS(const Graph& G, int v) {
+  seen[v] = true;
+
+  for (auto next_v : G[v]) {
+    if (seen[next_v]) {
+      continue;
+    }
+    DFS(G, next_v);
+  }
 }
