47d4dc123bf53a64eaab9f3d8f05a4f56d69d70c
ssmbc2929_bartok <ssmbc2929bartok@icloud.com>
2026-01-28 23:29:27 +0900

    change comments コメントアウトの形式を// -> /**/に変更 一部文面の最適化

--- algorithms/C/stack/stack.c
+++ algorithms/C/stack/stack.c
@@ -1,36 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// グローバル変数など
-#define G_STACK_SIZE 10         // stackの大きさ。必要に応じて変更してね
-typedef int stack;              // もしint型以外を使いたい方はここを変更してね
-stack gStack[G_STACK_SIZE];     // stack本体
-stack gStackTail = 0;           // stackの末尾（＝データサイズ）を表す
+/*グローバル変数など*/
+#define G_STACK_SIZE 10     /*stackの大きさ。必要に応じて変更してね*/
+typedef int stack;          /*もしint型以外を使いたいときはここを変更してね*/
+stack gStack[G_STACK_SIZE]; /*stack本体*/
+stack gStackTail = 0;       /*stackの末尾（＝データサイズ）を表す*/
 
-// 関数原型宣言
+/*関数原型宣言*/
 void error(char* scr);
 void push(stack inp);
 stack pop(void);
 
-// エラー操作を知らせて強制停止する関数
+/*エラー操作を知らせて強制停止する関数*/
 void error(char* scr) {
-  fprintf(stderr, scr);  // 標準エラー出力
-  exit(1);               // 異常強制終了
+  fprintf(stderr, scr); /*標準エラー出力*/
+  exit(1);              /*異常強制終了*/
 }
 
-//スタックにデータを積む(push)関数
+/*スタックにデータを積む(push)関数*/
 void push(stack inp) {
-  if (gStackTail == G_STACK_SIZE) {//gStackが範囲外参照になった場合はエラー出力
+  if (gStackTail == G_STACK_SIZE) { /*gStackが範囲外参照になった場合はエラー出力*/
     error("error:push:gStack is full!");
   }
   gStack[gStackTail++] = inp;
   return;
 }
 
-//スタックの一番上のデータを拾う(pop)関数
-stack pop(void){
-  if(gStackTail < 0){
-    error("error:pop:gStack is empty!");
+/*スタックの一番上のデータを拾う(pop)関数*/
+stack pop(void) {
+  if (gStackTail < 0) {
+    error("error:pop:gStack is empty!"); /*gStackが空の場合はエラー出力*/
   }
   return gStack[gStackTail--];
 }
