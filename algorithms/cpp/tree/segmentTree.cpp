ec4c10cbd767d05d8c3b77dc26f57268d72c66df
ssmbc2929_bartok <ssmbc2929bartok@icloud.com>
2026-02-11 14:34:50 +0900

    change : library ライブラリ化

--- algorithms/cpp/tree/segmentTree.cpp
+++ algorithms/cpp/tree/segmentTree.cpp
@@ -30,6 +30,16 @@ struct SegmentTree {
     }
   }
 
+  void build(const vector<ll>& v) {
+    for (int i = 0; i < (int)v.size(); i++) {
+      tree[n + i] = v[i];
+    }
+    // 下の段から順番に親を計算していく
+    for (int i = n - 1; i >= 1; i--) {
+      tree[i] = op(tree[i << 1], tree[i << 1 | 1]);
+    }
+  }
+
   // 区間クエリ: [l, r) の範囲を計算（半開区間）
   ll query(int l, int r) {
     ll res_l = unit;
@@ -41,3 +51,21 @@ struct SegmentTree {
     return op(res_l, res_r);
   }
 };
+
+// how to use
+/*
+// 最小値を求めたいとき（単位元は「十分大きい値」）
+SegmentTree st_min(n, LLONG_MAX, [](ll a, ll b){ return min(a, b); });
+
+// 最大値を求めたいとき（単位元は「十分小さい値」）
+SegmentTree st_max(n, LLONG_MIN, [](ll a, ll b){ return max(a, b); });
+
+// 和を求めたいとき（単位元は「0」）
+SegmentTree st_sum(n, 0, [](ll a, ll b){ return a + b; });
+
+// 3番目の要素（0-indexed）を 100 に変更
+st_min.update(3, 100);
+
+// 2番目から 5番目まで（[2, 6)）の最小値をゲット※半開区間
+ll result = st_min.query(2, 6);
+*/
